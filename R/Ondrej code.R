141eb237be9b3574f6f4eddfc50951e748c9dffd
midoriyajima <yajima.1651803@studenti.uniroma1.it>
2020-03-18 18:31:40 +0100

    Code adjustments

--- R/Ondrej code.R
+++ R/Ondrej code.R
@@ -24,10 +24,11 @@ str(Datasheet)
 
 ## number of sites per time bin
 ggplot(Datasheet)+
-  geom_bar(aes(x= reorder(Datasheet$Bin,-Datasheet$`Bin number`)))+
+  geom_bar(aes(x= reorder(Datasheet$Bin,-Datasheet$Bin_num)))+
   theme_bw()+
   theme(axis.text.x  = element_text(angle = 70, hjust=1))+
-  xlab("Time bins")
+  xlab("Time bins")+
+  ggtitle("Number of sites per time bin")
 
 ## number of records per time bin (Suzette)
 ggplot(Datasheet)+
@@ -73,27 +74,34 @@ DF.indicators.SUM <-data.frame(IN=apply(Datasheet.indicators, 2,
 DF.indicators.SUM$IN.name <- row.names(DF.indicators.SUM) %>% as.factor()
 
 DF.indicators.SUM %>%
+  filter(IN!=0)%>%
   ggplot(.,aes(x=reorder(IN.name,IN,FUN = max),y=IN)) +
-  drop_na() +
+  #drop_na() +
   geom_bar(stat = "identity") +
   theme(axis.text.x  = element_text(angle = 70, hjust=1)) +
-  xlab("Human Indicators") + ylab("Count")
+  geom_text(aes(label=IN), vjust=-0.5, size=2, hjust=0.3)+
+  xlab("Human Indicators") + ylab("Count")+
+  ggtitle("Number of bins where Indicators found")
 
 #### FIGURE 3 ---------------------------------
 ## number of times human indicators are counted in each site
 Datasheet.indicators.full %>% 
-  reshape2::melt %>%
-  drop_na()
+  reshape2::melt() %>%
+  group_by(`Site Name`, variable)%>%
+  drop_na()%>%
   summarise(Count = sum(value)) %>%
-  ggplot(aes(x = variable, y = Count)) +
+  filter(Count!=0)%>%
+  ggplot(aes(x = variable, y = Count))+
   geom_bar(stat = "identity")+
   facet_wrap(~`Site Name`)+
   theme(axis.text.x = element_text(angle = 90, hjust=1))
+
 
 ##how often indicators found in each site (showing sites per indicator)
 reshape2::melt(Datasheet.indicators.full) %>%
   group_by(variable,`Site Name`) %>%
   summarise(Count = sum(value)) %>%
+  filter(Count!=0)%>%
   ggplot(aes(x=`Site Name`,y=Count))+
   geom_bar(stat = "identity")+
   facet_wrap(~variable)+
@@ -113,7 +121,7 @@ DF.Direct<-apply(DF.Direct,2, FUN = function(x) as.numeric(x))
 #DF.Direct.full[,4:28]<-apply (DF.Direct.full[,4:28],2,FUN= function(x) as.numeric(unlist(x)))
 DF.Direct.Sum<-data.frame(SUM=apply(DF.Direct,1,FUN = function(x) sum(x,na.rm = T)))
 ##two ways to add direct indicators sum
-Datasheet.ind.sum<-bind_cols(Datasheet,DF.Direct.Sum)
+#Datasheet.ind.sum<-bind_cols(Datasheet,DF.Direct.Sum)
 Datasheet.ind.sum$Sum_direct<-DF.Direct.Sum$SUM
 
 ##for indirect
@@ -125,7 +133,7 @@ DF.Indirect.Sum<-data.frame(SUM=apply(DF.Indirect,1,FUN = function(x) sum(x,na.r
 Datasheet.ind.sum$Sum_indirect<-DF.Indirect.Sum$SUM
 
 #Absolute frequency of direct indicators through time per site
-ggplot(data=Datasheet.ind.sum,aes(x=reorder(Datasheet.ind.sum$Bin,-Datasheet.ind.sum$`Bin number`),y=Sum_direct))+
+ggplot(data=Datasheet.ind.sum,aes(x=reorder(Datasheet.ind.sum$Bin,-Datasheet.ind.sum$Bin_num),y=Sum_direct))+
   geom_bar(stat = "identity",width = 0.5)+
   facet_wrap(~`Site Name`)+
   theme_bw()+
@@ -133,9 +141,10 @@ ggplot(data=Datasheet.ind.sum,aes(x=reorder(Datasheet.ind.sum$Bin,-Datasheet.ind
   geom_text(aes(label=Sum_direct), vjust=-0.3, size=3)+
   ylab("Absolute frequency")+
   xlab("Time bins")
+  
 
 ##Absolute frequency of indirect indicators through time per site
-ggplot(data=Datasheet.ind.sum,aes(x=reorder(Datasheet.ind.sum$Bin,-Datasheet.ind.sum$`Bin number`),y=Sum_indirect))+
+ggplot(data=Datasheet.ind.sum,aes(x=reorder(Datasheet.ind.sum$Bin,-Datasheet.ind.sum$Bin_num),y=Sum_indirect))+
   geom_bar(stat = "identity",width = 0.5)+
   facet_wrap(~`Site Name`)+
   theme_bw()+
@@ -146,7 +155,7 @@ ggplot(data=Datasheet.ind.sum,aes(x=reorder(Datasheet.ind.sum$Bin,-Datasheet.ind
   
 ##for a single site
 Datasheet.ind.sum%>%filter(`Site Name`=="Pantano de Genagra")%>%
-  ggplot(aes(x=reorder(`Bin`,-`Bin number`),y=Sum_indirect))+
+  ggplot(aes(x=reorder(`Bin`,-Bin_num),y=Sum_indirect))+
   geom_bar(stat = "identity",width = 0.9)+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, hjust=1))+
@@ -155,14 +164,14 @@ Datasheet.ind.sum%>%filter(`Site Name`=="Pantano de Genagra")%>%
   xlab("Time bins")
   ggtitle("Pantano de Genagra")
 
-##to have a single chart for direct and indirect indicators
+##to have a single figure for direct and indirect indicators
 DF.Indirect.Sum<-DF.Indirect.Sum%>%mutate(IND=rep("Indirect"))
 DF.Direct.Sum<-DF.Direct.Sum%>%mutate(IND=rep("Direct"))
 Datasheet.D.full<-bind_cols(Datasheet,DF.Direct.Sum)
 Datasheet.I.full<-bind_cols(Datasheet,DF.Indirect.Sum)
 Datasheet.full<-rbind(Datasheet.D.full,Datasheet.I.full)
   
-ggplot(data=Datasheet.full,aes(x=reorder(Datasheet.full$Bin,-Datasheet.full$`Bin number`),y=SUM,fill=IND))+
+ggplot(data=Datasheet.full,aes(x=reorder(Datasheet.full$Bin,-Datasheet.full$Bin_num),y=SUM,fill=IND))+
   geom_bar(position="dodge",stat = "identity",width = 0.5)+
   facet_wrap(~`Site Name`)+
   theme_bw()+
@@ -174,7 +183,7 @@ ggplot(data=Datasheet.full,aes(x=reorder(Datasheet.full$Bin,-Datasheet.full$`Bin
 ##for a single site
 #dodged bars
 Datasheet.full%>%filter(`Site Name`=="Pantano de Genagra")%>%
-  ggplot(aes(x=reorder(`Bin`,-`Bin number`),y=SUM,fill=IND))+
+  ggplot(aes(x=reorder(`Bin`,-Bin_num),y=SUM,fill=IND))+
   geom_bar(stat = "identity",width = 0.9, position = "dodge")+
   geom_text(aes(label=SUM), vjust=-0.3, size=3, position = "dodge")+
   theme_bw()+
@@ -185,7 +194,7 @@ ggtitle("Pantano de Genagra")
 
 #stacked bars
 Datasheet.full%>%filter(`Site Name`=="Pantano de Genagra")%>%
-  ggplot(aes(x=reorder(`Bin`,-`Bin number`),y=SUM,fill=IND))+
+  ggplot(aes(x=reorder(`Bin`,-Bin_num),y=SUM,fill=IND))+
   geom_bar(stat = "identity",width = 0.9, position = "stack")+
   geom_text(aes(label=SUM), vjust=0.5, size=3, position = "stack")+
   theme_bw()+
@@ -196,7 +205,7 @@ Datasheet.full%>%filter(`Site Name`=="Pantano de Genagra")%>%
 
 #stacked bars with the same height
 Datasheet.full%>%filter(`Site Name`=="Pantano de Genagra")%>%
-  ggplot(aes(x=reorder(`Bin`,-`Bin number`),y=SUM,fill=IND))+
+  ggplot(aes(x=reorder(`Bin`,-Bin_num),y=SUM,fill=IND))+
   geom_bar(stat = "identity",width = 0.9, position = "fill")+
   geom_text(aes(label=SUM), vjust=0.5, size=3, position = "fill")+
   theme_bw()+
