7d42d969af85e0eccec694c1a3af9294bd2ccd60
midoriyajima <yajima.1651803@studenti.uniroma1.it>
2020-03-30 19:25:23 +0200

    Code re-run with new Datasheet (and minor adjustments)

--- R/Map.R
+++ R/Map.R
@@ -12,7 +12,7 @@ LAPD_Andes <- read_excel("data/LAPD_Andes_MY.xlsx")
 LAPD_Andes$LAPD_ID<-as.numeric(LAPD_Andes$LAPD_ID)
 
 #change name to LAPD_ID column, easier to handle
-names(Datasheet)[names(Datasheet)=="LAPD_ID\r\n"]<-"LAPD_ID"
+names(Datasheet)[names(Datasheet)=="LAPD_ID\n"]<-"LAPD_ID"
 
 #select sites from LAPD_andes_final that are in Datasheet 
 DS<-Datasheet%>%filter(LAPD_ID!="NA")%>%select(LAPD_ID)%>%distinct(LAPD_ID) #excluding sites NA (sites further added to Datasheet which were not present in LAPD_Andes_final)
@@ -21,7 +21,7 @@ A=LAPD_Andes[LAPD_Andes$LAPD_ID %in% DS, ]
 
 #add Country, Lat, Long to NA Sites (sites that were absent in LAPD_andes_final)
 na_id=Datasheet%>%filter(LAPD_ID=="NA")
-na_sites<-data.frame(c("Tres Lagunas","El Triunfo","Anteojos Valley","Huila","Paramo de Agua Blanca III", "Paramo de Laguna Verde I","Paramo de Laguna Verde II","Paramo de pena Negra I" ),
+na_sites<-data.frame(c("Tres Lagunas","El Triunfo","Anteojos Valley","Huila\n","Agua Blanca PAB III", "Paramo de Laguna Verde I","Paramo de Laguna Verde II","Paramo de Pena Negra 1" ),
                      c("Ecuador","Colombia","Ecuador","Ecuador","Colombia","Colombia","Colombia","Colombia"),
                      c(-3.05145,4.58545,-0.57946,-00.25405,5.00000,5.15000,5.15000,5.05000),
                      c(-79.24822,-75.19558,-78.24397,-78.01075,-74.10000,-74.00000,-74.00000,-74.05000))
@@ -42,14 +42,14 @@ Datasheet$Longitude<-A$Longitude[match(Datasheet$`Site Name`,A$SiteName)]
 Sitename=distinct(Datasheet,`Site Name`)
 Nomatch<-match(Sitename$`Site Name`,LAPD_Andes$SiteName)
 which(is.na(Nomatch))
-Sites_Nomatch<-Datasheet[c(3,7,10,13,22,32,33,36),]
+Sites_Nomatch<-Sitename[c(7,10,13,22,32,33,36),]
 Sites_Nomatch$`Site Name`
 LAPD_Andes$SiteName
 #rename Datasheet Site Names according to the ones in LAPD_Andes
-Datasheet$`Site Name`[Datasheet$`Site Name` %in% c("La Laguna\r\n" , "Cerro Toledo","El Tiro\r\n","Laguna Natosas forest", "Pedras Blancas"  , "Anteojos Valley" , "Paramo de Laguna Verde II")]<-c("La Laguna","Cerro Toledo CT","El Tiro" ,"Lagunas Natosas Forest","Piedras Blancas","Anteojos Valley" ,"Paramo de Laguna Verde II")
+Datasheet$`Site Name`[Datasheet$`Site Name` %in% c("Cerro Toledo", "El Tiro\n" , "Laguna Natosas forest", "Pedras Blancas","Huila", "Paramo de Agua Blanca III", "Paramo de pena Negra I"   )]<-c("Cerro Toledo CT", "El Tiro", "Lagunas Natosas Forest", "Piedras Blancas", "Huila\n", "Agua Blanca PAB III", "Paramo de Pena Negra 1")
+#Repeat (1)
 #see if there something NA left, and which site it corresponds to
 view(filter(Datasheet,is.na(Country)))
-#Repeat (1)
 
 ##Step 2: tableinstall.packages("formattable")
 install.packages("formattable")
@@ -70,9 +70,10 @@ write_xlsx(Datasheet,"Datasheet.xlsx")
 library(OpenStreetMap)
 library(ggmap)
 
+Sites<-Datasheet%>%distinct(`Site Name`,Latitude,Longitude)
+
 range(Sites$Longitude)
 range(Sites$Latitude)
-Sites<-Datasheet%>%distinct(`Site Name`,Latitude,Longitude)
 
 Andes<-get_stamenmap(bbox = c(left=-80,
                               bottom=-5,
