e01a32297f9f97a33ee1bebc16747b646b1d6040
Ondřej Mottl <ondrej.mottl@gmail.com>
2020-05-20 14:51:05 +0200

    Update: small eddit to the calculation of clusters (now data are standardise and normalise) -> new clustering result!

--- R/01_site_clustering.R
+++ R/01_site_clustering.R
@@ -2,8 +2,11 @@
 #               SITE CLUSTERING BASED ON THE GEOGRAPHY
 # ---------------------------------------------------------------
 library(tidyverse)
+library(readxl)
 #install.packages("NbClust")
 library(NbClust)
+library(RColorBrewer)
+library(ggmap)
 
 Datasheet_shaved <- read_excel("data/Datasheet_shaved.xlsx")
 Sites<-Datasheet_shaved %>% 
@@ -12,17 +15,17 @@ Sites<-Datasheet_shaved %>%
 
 # set the best number of clusters based on the location and elevation (max 10) 
 clusters <- Sites %>%
-  mutate(Altitude.log = log(Altitude)) %>%
-  dplyr::select(Latitude,Longitude,Altitude.log) %>%
-  NbClust(., min.nc = 3, max.nc = 10, method = "complete")
-
-# ***** Conclusion *****                            
-#* According to the majority rule, the best number of clusters is  4 
+  mutate(long.s = scale(Longitude),
+         lat.s = scale(Latitude),
+         elev.s = scale(Altitude)) %>%
+  dplyr::select(long.s, lat.s, elev.s) %>%
+  NbClust(., min.nc = 3, max.nc = 10, method = "kmeans")
 
 # add the cluster definition to the Sites DF
 Sites$cluster <- as.factor(clusters$Best.partition)
 
-Color.legen <- brewer.pal(n = 4, name = 'Set2')
+# create colors pallete for each of the cluster
+Color.legen <- brewer.pal(n = levels(Sites$cluster) %>% length(), name = 'Set2')
 names(Color.legen) <- levels(Sites$cluster)
 
 # check the distribution of suggested separation
@@ -36,6 +39,7 @@ ggmap(get_stamenmap(bbox = c(left=-84,
                              color=cluster, 
                              size=Altitude))+
   scale_color_manual(values= Color.legen)+
+  guides(color=F)+
   labs(y="Latitude",
        x="Longitude")
 
